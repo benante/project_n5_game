6c1d0744e8b9c56b65b9a7be9eac2014e8efdcc9
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2022-11-22 17:30:59 +0000

    Almost done, needs some retouching

--- app.js
+++ app.js
@@ -5,9 +5,6 @@ const combinations = {
 };
 const items = ["rock", "paper", "scissors"];
 
-let user_score = 0;
-let pc_score = 0;
-
 let game = true;
 
 const selection_imgs = document.querySelectorAll(".user_pic");
@@ -21,13 +18,12 @@ selection_imgs.forEach((img) => {
       let user_item = img.id;
       let selectedItem = document.getElementById(user_item);
       selectedItem.classList.add("highlight");
-
-      // This need to go inside timeout
+      roll_dice()
       setTimeout(() => {
         let computer_item = items[random()];
         let result = scoring(combinations, user_item, computer_item);
         document.querySelector("span").innerHTML = result;
-      }, 2000);
+      }, 1050);
     }
     game = false;
   });
@@ -44,18 +40,22 @@ button.addEventListener("click", () => {
 });
 
 // FUNCTIONS
+
+// Check result
 function scoring(combinations, user_item, computer_item) {
   console.log("User chose " + user_item);
   console.log("computer chose " + computer_item);
   return combinations[user_item][computer_item];
 }
+
+// Get random computer choice 
 function random() {
-  pic.classList.toggle("hide");
   let random = Math.floor(Math.random() * 3);
   pic.src = "/static/" + items[random] + ".png";
   return random;
 }
 
+// Simple "rolling dice" visual effect
 function roll_dice() {
   let image = document.getElementById("random");
   image.classList.toggle("hide");
@@ -66,6 +66,4 @@ function roll_dice() {
   setTimeout(function () {
     clearInterval(temporary_roll);
   }, 1000);
-
-  return random;
 }
