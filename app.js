86d7a459509fe2a0b5b5535c8021b6ae864e8266
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-03-24 22:01:09 +0000

    Refactored

--- app.js
+++ app.js
@@ -7,21 +7,23 @@ const items = ["rock", "paper", "scissors"];
 
 let game = true;
 
-const selection_imgs = document.querySelectorAll(".user_pic");
-const random_pic = document.getElementById("random");
-const button = document.querySelector("button");
-const pic = document.getElementById("random");
+const userOptions = document.querySelectorAll(".user_pic");
+const restartButton = document.querySelector("button");
+const randomSelection = document.getElementById("random");
 const computer = document.getElementById("computer");
 
-selection_imgs.forEach((img) => {
+// Start game when user clicks on icon
+userOptions.forEach((img) => {
   img.addEventListener("click", () => {
     if (game == true) {
       let user_item = img.id;
       let selectedItem = document.getElementById(user_item);
       selectedItem.classList.add("highlight");
+      // Start visual rolling dice function
       roll_dice();
+      // Get the actual randomized item, display the corresponding icon and the result after 1050 milliseconds
       setTimeout(() => {
-        let computer_item = items[random()];
+        let computer_item = items[randomize()];
         let result = scoring(combinations, user_item, computer_item);
         document.querySelector("span").innerHTML = result;
       }, 1050);
@@ -30,10 +32,11 @@ selection_imgs.forEach((img) => {
   });
 });
 
-button.addEventListener("click", () => {
+// If game is done (false), reset icons and message
+restartButton.addEventListener("click", () => {
   if (game == false) {
     computer.classList.toggle("hide");
-    pic.classList.toggle("hide");
+    randomSelection.classList.toggle("hide");
     let remove = document.getElementsByClassName("highlight")[0];
     remove.classList.remove("highlight");
     document.querySelector("span").innerHTML = "VS";
@@ -45,27 +48,25 @@ button.addEventListener("click", () => {
 
 // Check result
 function scoring(combinations, user_item, computer_item) {
-  console.log("User chose " + user_item);
-  console.log("computer chose " + computer_item);
   return combinations[user_item][computer_item];
 }
 
-// Get random computer choice
-function random() {
+// Get random computer choice and corresponding item (with its img)
+function randomize() {
   let random = Math.floor(Math.random() * 3);
-  pic.src = "./static/" + items[random] + ".png";
+  randomSelection.src = "./static/" + items[random] + ".png";
   return random;
 }
 
 // Simple "rolling dice" visual effect
 function roll_dice() {
-  // let image = document.getElementById("random");
   computer.classList.toggle("hide");
-  pic.classList.toggle("hide");
+  randomSelection.classList.toggle("hide");
+  // Call randomize() every 0.04 seconds intervals with setInterval
   let temporary_roll = setInterval(function () {
-    let random = Math.floor(Math.random() * 3);
-    pic.src = "./static/" + items[random] + ".png";
+    randomize();
   }, 40);
+  // ClearInterval interrupts the function after 1 second with setTimeout
   setTimeout(function () {
     clearInterval(temporary_roll);
   }, 1000);
